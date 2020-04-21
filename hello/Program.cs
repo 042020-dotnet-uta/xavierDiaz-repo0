0d12a9b3d550fb1125b505e367261d50d10fdd44
Xavier Diaz <xdiaz@ucsc.edu>
2020-04-21 09:33:20 -0700

    added loop

--- hello/Program.cs
+++ hello/Program.cs
@@ -7,6 +7,12 @@ namespace hello
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Universes!");
+            int i = 20;
+            while(i > 0){
+                Console.WriteLine("test "+i);
+                i = i - 1;
+            }
         }
     }
 }
